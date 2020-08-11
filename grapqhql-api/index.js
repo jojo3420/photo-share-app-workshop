fd3917a47bcd219e484989bf61c75339c98f99a5
jojo3420 <jjjhhhvvv@gmail.com>
2020-08-11 16:41:17 +0900

    backend: 프로젝트 re Setup  1. install: apollo-server-express, express, mongodb, dotenv,              graphql-playground-middleware-express  2. express + apollo-server-express 이용한 서버 구현  3. mongo db 연결  4. .env 설정 및 github auth 정보 추가  5. 프로젝트 구조 변경

---
+++ grapqhql-api/index.js
@@ -0,0 +1,66 @@
+const express = require('express');
+const { ApolloServer } = require('apollo-server-express');
+const { MongoClient } = require('mongodb');
+const expressPlayground = require('graphql-playground-middleware-express')
+  .default;
+const resolvers = require('./resolvers/resolvers');
+const { readFileSync } = require('fs');
+
+// env
+require(`dotenv`).config();
+
+// 'UTF-8' encoding 중요!
+const typeDefs = readFileSync('./typeDefs.graphql', 'UTF-8');
+
+async function run() {
+  const app = express();
+  const MONGO_DB = process.env.DB_HOST;
+  let db;
+  // console.log({ MONGO_DB });
+  try {
+    const client = await MongoClient.connect(MONGO_DB, {
+      useNewUrlParser: true,
+    });
+    db = client.db();
+  } catch (e) {
+    console.log(`
+      Mongo DB Host not found!
+      please add DB_HOST environment variable to .env file
+
+      exiting...
+    `);
+  }
+
+  const context = { db };
+
+  const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+    context,
+  });
+
+  // apply express
+  server.applyMiddleware({ app });
+
+  // playground router
+  app.get('/playground', expressPlayground({ endpoint: '/resolvers' }));
+
+  // home
+  app.get('/', (req, res) => {
+    let url = `https://github.com/login/oauth/authorize?client_id=${process.env.CLIENT_ID}&scope=user`;
+    res.end(`<a href="${url}">Sign In with Github</a>`);
+  });
+
+  app.listen({ port: 4000 }, (err) => {
+    if (err) {
+      console.log(`
+       server error ${err.toString()}
+      `);
+    }
+    console.log(
+      `GraphQL Server running at http://localhost:4000${server.graphqlPath}`,
+    );
+  });
+}
+
+run();
--- grapqhql-api/resolvers/resolvers.js
+++ grapqhql-api/resolvers/resolvers.js
@@ -1,7 +1,6 @@
 const { GraphQLScalarType } = require('graphql');
 const { photos, tags, users } = require('./db');
 
-
 let _id = 5;
 
 const resolvers = {
@@ -21,7 +20,7 @@ const resolvers = {
         id: _id.toString(),
         ...props.input,
         created: new Date(),
-        githubUser: 'jojo' // @TODO: 작성자 임시로 하드 코딩
+        githubUser: 'jojo', // @TODO: 작성자 임시로 하드 코딩
       };
       // console.log({ photo })
       photos.push(photo);
@@ -40,14 +39,17 @@ const resolvers = {
       // console.log({ user });
       return user;
     },
-    taggedUsers: (parent) => {  // parent is photo
-      return tags
-        // 전체 태그 목록에서 현재 사진의 태그만 추린다.
-        .filter(tag => tag.photoID === parent.id)
-        // 추린 태그 목록에서 유저 아이디로만 되어있는 배열을 만든 후.
-        .map(tag => tag.userID)
-        // 이 유저 아이디 배열과 일치 하는 유저 목록을  만든다.
-        .map(userID => users.find(user => user.githubLogin === userID))
+    taggedUsers: (parent) => {
+      // parent is photo
+      return (
+        tags
+          // 전체 태그 목록에서 현재 사진의 태그만 추린다.
+          .filter((tag) => tag.photoID === parent.id)
+          // 추린 태그 목록에서 유저 아이디로만 되어있는 배열을 만든 후.
+          .map((tag) => tag.userID)
+          // 이 유저 아이디 배열과 일치 하는 유저 목록을  만든다.
+          .map((userID) => users.find((user) => user.githubLogin === userID))
+      );
       // 최종적으로 해당 포토에 태그된 유저 목록을 리턴한다.
     },
   },
@@ -55,26 +57,29 @@ const resolvers = {
     postedPhotos: (parent) => {
       return photos.filter((photo) => photo.githubUser === parent.githubLogin);
     },
-    inPhotos: (parent) => { // parent is user
-      return tags
-        // 전체 태그 목록 에서 일단 현재 유저의 태그 목록만 추린다.
-        .filter(tag => tag.userID === parent.githubLogin)
-        // 추린 태그 목록으로  포토 아이디 로만 구성된 베열을 만든 후
-        .map(tag => tag.photoID)
-        //  포토 아이디 배열의 값과 일치 하는 포토들만 필터링 한다.
-        .map(photoID => photos.find(photo => photo.id === photoID))
+    inPhotos: (parent) => {
+      // parent is user
+      return (
+        tags
+          // 전체 태그 목록 에서 일단 현재 유저의 태그 목록만 추린다.
+          .filter((tag) => tag.userID === parent.githubLogin)
+          // 추린 태그 목록으로  포토 아이디 로만 구성된 베열을 만든 후
+          .map((tag) => tag.photoID)
+          //  포토 아이디 배열의 값과 일치 하는 포토들만 필터링 한다.
+          .map((photoID) => photos.find((photo) => photo.id === photoID))
+      );
       // 최종적 으로 해당 유저가 태그된 포토 목록을 리턴한다.
-    }
+    },
   },
   DateTime: new GraphQLScalarType({
     name: 'DateTime',
-    description: "A valid date time value",
+    description: 'A valid date time value',
     // 2. 쿼리 인자값을 js Date 타입으로 파싱 한다.
-    parseValue: value => new Date(value),
+    parseValue: (value) => new Date(value),
     // 3. 값일 내 보낼때 ISO 변환 후 내 보낸다.
-    serialize: value => new Date(value).toISOString(),
+    serialize: (value) => new Date(value).toISOString(),
     // 1. (옵션)파싱전에 인자값 을 가져 오거나 (인자가 리터럴 일 경우!)
-    parseLiteral: ast => ast.value,
+    parseLiteral: (ast) => ast.value,
   }),
 };
 
